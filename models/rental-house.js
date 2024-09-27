ec3a6a70268bf30ee3660a4ddbfc52fcc9ae7970
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-27 12:48:41 -0400

    implement rental house controllers

--- models/rental-house.js
+++ models/rental-house.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const rentalHouseSchema = new Schema({
   name: { type: String, required: true },
+  job: { type: Schema.Types.ObjectId, ref: "Job" },
 });
 
 const RentalHouse = mongoose.model("RentalHouse", rentalHouseSchema);
--- routes/rental-houses.js
+++ routes/rental-houses.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as controllers from "../controllers/rental-houses.js";
 import verifyToken from "../middleware/verify-token.js";
 import isAuthorized from "../middleware/isAuthorized.js";
-const router = Router();
+const router = Router({ mergeParams: true });
 
 // Apply `verifyToken` middleware to all routes
 router.use(verifyToken);
@@ -10,8 +10,8 @@ router.use(verifyToken);
 router.post("/", controllers.createRentalHouse);
 router.get("/", controllers.getAllJobRentalHouses);
 
-router.get("/:walkieId", isAuthorized, controllers.getRentalHouse);
-router.put("/:walkieId", isAuthorized, controllers.updateRentalHouse);
-router.delete("/:walkieId", isAuthorized, controllers.deleteRentalHouse);
+router.get("/:rentalHouseId", isAuthorized, controllers.getRentalHouse);
+router.put("/:rentalHouseId", isAuthorized, controllers.updateRentalHouse);
+router.delete("/:rentalHouseId", isAuthorized, controllers.deleteRentalHouse);
 
 export default router;
