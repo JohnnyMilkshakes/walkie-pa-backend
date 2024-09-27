dd85838f0b89a76639079d0d2073861edd363724
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-27 16:56:02 -0400

    complete walkie controllers, adjust routes to make more sense, the route to create a walkie now lives in the index of the routes folder since it is unique and special <3

--- controllers/walkies.js
+++ controllers/walkies.js
@@ -1,13 +1,10 @@
-import Job from "../models/job.js";
 import Walkie from "../models/walkie.js";
 import RentalHouse from "../models/rental-house.js";
 import CrewMember from "../models/crew-member.js";
-import User from "../models/user.js";
 import Department from "../models/department.js";
 
 export const getWalkie = async (req, res) => {
   try {
-    console.log(req.params.jobId);
     const walkie = await Walkie.findById(req.params.walkieId);
 
     if (walkie) {
@@ -22,8 +19,16 @@ export const getWalkie = async (req, res) => {
 
 export const getAllJobWalkies = async (req, res) => {
   try {
-    const userId = req.user._id;
-    const walkies = await Walkie.find({ createdBy: userId });
+    const { jobId } = req.params;
+    const rentalHouses = await RentalHouse.find({ job: jobId })
+      .select("_id")
+      .lean();
+    const rentalHouseIds = rentalHouses.map((rentalHouse) => rentalHouse._id);
+
+    // This query will match all walkies associated with any rental house in the array of rental house ID's passed to it
+    const walkies = await Walkie.find({
+      rentalHouse: { $in: rentalHouseIds },
+    });
     res.status(201).json(walkies);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -31,9 +36,9 @@ export const getAllJobWalkies = async (req, res) => {
 };
 
 export const createWalkie = async (req, res) => {
-  const rentalId = req.body.rentalHouseId;
-  const departmentId = req.body.departmentId;
-  const crewMemberId = req.body.crewMemberId;
+  const rentalId = req.params.rentalHouseId;
+  const { departmentId } = req.body;
+  const { crewMemberId } = req.body;
 
   try {
     let rentalHouse = await RentalHouse.findById(rentalId);
@@ -43,7 +48,7 @@ export const createWalkie = async (req, res) => {
       req.body.rentalHouse = rentalId;
     }
 
-    // Department is optional 
+    // Department is optional
     if (departmentId) {
       let department = await Department.findById(departmentId);
       if (!department) {
@@ -53,7 +58,7 @@ export const createWalkie = async (req, res) => {
       }
     }
 
-    // Crew Member is optional 
+    // Crew Member is optional
     if (crewMemberId) {
       let crewMember = await CrewMember.findById(crewMemberId);
       if (!crewMember) {
@@ -63,7 +68,7 @@ export const createWalkie = async (req, res) => {
       }
     }
 
-    const walkie = Walkie.create(req.body);
+    const walkie = await Walkie.create(req.body);
     res.status(201).json(walkie);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -72,23 +77,45 @@ export const createWalkie = async (req, res) => {
 
 export const updateWalkie = async (req, res) => {
   try {
-    const { jobId } = req.params;
+    const { walkieId } = req.params;
+    const { departmentId } = req.body;
+    const { crewMemberId } = req.body;
 
-    const job = await Job.findById(jobId);
+    const walkie = await Walkie.findById(walkieId);
 
-    // If job is not found, return a 404 error
-    if (!job) {
-      return res.status(404).json({ error: "Job not found" });
+    // If walkie is not found, return a 404 error
+    if (!walkie) {
+      return res.status(404).json({ error: "Walkie not found" });
     }
 
-    // Update the job fields with the data from req.body
-    Object.assign(job, req.body);
+    // Department is optional
+    if (departmentId) {
+      let department = await Department.findById(departmentId);
+      if (!department) {
+        res.status(400).json({ message: "Department not found" });
+      } else {
+        req.body.department = departmentId;
+      }
+    }
 
-    // Save the updated job back to the database
-    const updatedJob = await job.save();
+    // Crew Member is optional
+    if (crewMemberId) {
+      let crewMember = await CrewMember.findById(crewMemberId);
+      if (!crewMember) {
+        res.status(400).json({ message: "Crew Member not found" });
+      } else {
+        req.body.crewMember = crewMemberId;
+      }
+    }
 
-    // Send the updated job as the response
-    res.status(200).json(updatedJob);
+    // Update the walkie fields with the data from req.body
+    Object.assign(walkie, req.body);
+
+    // Save the updated walkie back to the database
+    const updatedWalkie = await walkie.save();
+
+    // Send the updated walkie as the response
+    res.status(200).json(updatedWalkie);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
@@ -97,14 +124,14 @@ export const updateWalkie = async (req, res) => {
 
 export const deleteWalkie = async (req, res) => {
   try {
-    const { jobId } = req.params;
-    const deleted = await Job.findByIdAndDelete(jobId);
+    const { walkieId } = req.params;
+    const deleted = await Walkie.findByIdAndDelete(walkieId);
 
     if (deleted) {
-      return res.status(200).send("Job Deleted!");
+      return res.status(200).send("Walkie Deleted!");
     }
 
-    throw new Error("Job not found");
+    throw new Error("Walkie not found");
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
--- routes/walkies.js
+++ routes/walkies.js
@@ -2,14 +2,12 @@ import { Router } from "express";
 import * as controllers from "../controllers/walkies.js";
 import verifyToken from "../middleware/verify-token.js";
 import isAuthorized from "../middleware/isAuthorized.js";
-const router = Router();
+const router = Router({ mergeParams: true });
 
 // Apply `verifyToken` middleware to all routes
 router.use(verifyToken);
 
-router.post("/", controllers.createWalkie);
-router.get("/", controllers.getAllJobWalkies);
-
+router.get("/", isAuthorized, controllers.getAllJobWalkies);
 router.get("/:walkieId", isAuthorized, controllers.getWalkie);
 router.put("/:walkieId", isAuthorized, controllers.updateWalkie);
 router.delete("/:walkieId", isAuthorized, controllers.deleteWalkie);
